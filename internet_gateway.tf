00ca575fc5d0f550186719e2af8b1f0dcb059a27
EC2 Default User <ec2-user@ip-172-31-35-244.ap-northeast-1.compute.internal>
2019-05-07 07:56:12 +0000

    first commit

---
+++ internet_gateway.tf
@@ -0,0 +1,8 @@
+resource "aws_internet_gateway" "dev" {
+  vpc_id = "${aws_vpc.dev.id}"
+
+  tags {
+    Name = "dev17"
+  }
+}
+
---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "amazon_linux" {
+  # amzn-ami-hvm-2018.03.0.20181129-x86_64-gp2 (ami-00a5245b4816c38e6)
+  default = "ami-00a5245b4816c38e6"
+}
+
+variable "dev_keyname" {
+  default = "user17-keypair"
+}
+
